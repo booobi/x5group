88cddc16325030748f8460dab0405d866be72618
booobi <34624690+booobi@users.noreply.github.com>
2020-11-21 20:08:26 +0200

    update to angular 11

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -18,7 +18,7 @@ const routes: Route[] = [
     ],
     imports: [
         CommonModule,
-        RouterModule.forRoot(routes),
+        RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
         MatCardModule,
         MatButtonModule,
     ],
